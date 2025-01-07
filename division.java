9a0f787dad38d2c66e05be1536df927340533fa8
sakshi02v <sakshi02v@gmail.com>
2025-01-07 12:37:59 +0530

    functionality added

---
+++ division.java
@@ -0,0 +1,5 @@
+public class division {
+    public static double add(double a, double b) {
+        return a / b;
+    
+}}
---
+++ multiplication.java
@@ -0,0 +1,6 @@
+public class multiplication {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    
+}
